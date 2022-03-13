545d3a597d63da2386bb7bd4ea08053d3c3106e8
pranavismyname <pranavlad007@gmail.com>
2022-03-13 16:37:55 -0700

    new tc added

---
+++ src/main/java/pomClasses/KiteLogoutPage.java
@@ -0,0 +1,23 @@
+package pomClasses;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class KiteLogoutPage {
+	@Test(groups ="buy")
+	public void tc04() {
+		Reporter.log("tc04 is passed");
+	}
+	@Test(groups ="buy")
+	public void tc05() {
+		Reporter.log("tc05 is passed");
+	}
+	@Test(groups ="buy")
+	public void tc06() {
+		Reporter.log("tc06 is passed");
+	}
+	@Test(groups ="sell")
+	public void tc07() {
+		Reporter.log("tc07 is passed");
+	}
+}
